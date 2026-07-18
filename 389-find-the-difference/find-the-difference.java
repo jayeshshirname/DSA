ef8c144bcede5000c1a302b376bdebca1b60cec2
Jayesh Shirname <rakeshshirname65381@gmail.com>
2026-07-18 20:32:28 +0530

    Time: 7 ms (17.20%) | Memory: 43 MB (85.27%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+
+        StringBuilder sb = new StringBuilder(t);
+        int n1 = s.length();
+        int n2 = s.length();
+
+        for(int i=0;i<n1;i++) {
+            char ch = s.charAt(i);
+            for(int j=0; j<n2;j++) {
+                if (ch == sb.charAt(j)) {
+                    sb.deleteCharAt(j);
+                    break;
+                }
+            }
+        }
+        return sb.charAt(0);
+    }
+}
