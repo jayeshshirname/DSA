e619431019d3ab7284fad1990e71bb35145f3099
Jayesh Shirname <rakeshshirname65381@gmail.com>
2026-08-07 19:53:12 +0530

    Time: 1 ms (99.95%) | Memory: 77.1 MB (83.72%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,9 +3,10 @@ class Solution {
 
         int currentSum = arr[0];
         int maxSum = arr[0];
-        for(int i = 1; i < arr.length; i++) {
+
+        for(int i=1;i<arr.length;i++){
             currentSum = Math.max(arr[i], currentSum + arr[i]);
-            maxSum = Math.max(maxSum, currentSum);
+            maxSum = Math.max(maxSum,currentSum);
         }
         return maxSum;
     }
